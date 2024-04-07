2f77070410f20ec388fa0a6b15b60baa69ac93d0
AM0312 <ansh.midha0312@gmail.com>
2024-04-07 12:11:00 +0530

    Update test.cy.js

--- test.cy.js
+++ test.cy.js
@@ -3,6 +3,6 @@
 describe("template spec", () => {
   it("passes", () => {
     cy.visit("https://am0312.github.io/Pipeline-Practice-2.0/");
-    cy.get("h1").should("have.text", "\bMagic\b");
+    cy.get("h1").should("contain.text", "Magic");
   });
 });
