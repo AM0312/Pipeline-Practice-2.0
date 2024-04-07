dde48e7f11d8d370585074416568d1c4a4183503
AM0312 <ansh.midha0312@gmail.com>
2024-04-07 11:48:01 +0530

    cypress added

---
+++ test.cy.js
@@ -0,0 +1,8 @@
+/// <reference types="cypress" />
+
+describe("template spec", () => {
+  it("passes", () => {
+    cy.visit("https://am0312.github.io/Pipeline-Practice-2.0/");
+    cy.get("h1").should("have.text", "\bMagic\b");
+  });
+});
--- testing.js
+++ testing.js
@@ -1,16 +1,1 @@
-function printStarPattern(rows) {
-  let pattern = "";
-
-  for (let i = 1; i <= rows; i++) {
-    for (let j = 1; j <= i; j++) {
-      pattern += "* ";
-    }
-    pattern += "\n";
-  }
-
-  console.log(pattern);
-}
-
-// Change the number of rows as needed
-const numRows = 50;
-printStarPattern(numRows);
+const puppeteer = require("puppeteer");
